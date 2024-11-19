305ebf22032af0ddc4d60275b670534002e41323
Sam Glassman <samglassman@outlook.com>
2024-11-19 18:57:30 +0100

    start task 1

--- code/lEcuyer.m
+++ code/lEcuyer.m
@@ -1,3 +1,9 @@
 function u = lEcuyer(z1, z2, z3, z4, n)
-    
+    % Where n is the number of random numbers to be generated, and z1,  z2,  z3, and z4 the seeds. The output u is a nx1 vector of U(0,1) random numbers.  
+    % Toolboxes: For task 1 a) you are permitted to use the 
+    % - bitsll
+    % - bitsrl
+    % - bitand
+    % - bitxor
+    % functions from the Fixed-Point Designer Toolbox. 
 end
--- code/task1.m
+++ code/task1.m
@@ -1,2 +1,36 @@
 % Add your code for Task 1 in this file
 
+% Task 1
+% ---
+
+clearvars; clcl
+
+% In (L'Ecuyer, 1999), several approaches for combined LFSRs are given. In this task we will 
+% restrict ourselves to the first example of ME-CF generators as indicated in Table 1 of this paper. 
+% We make the following choices for the parameters: 
+L = 32;
+I = 4;
+s1 = 18;
+s2 = 2; 
+s3 = 7;
+s4 = 13;
+s = [s1, s2, s3, s4];
+% Furthermore, we take as seed 𝑧 = 2957, 𝑧 = 646, 𝑧 = 3847, 𝑧 = 947. 
+z1 = 2957; z2 = 646; z3 = 3847; z4 = 947;
+z = [z1, z2, z3, z4];
+% (1a) Implement this combined generator in MATLAB and generate 10,000 random numbers with this generator.  
+
+% initialize parameters
+z1 = 2957;
+z2 = 646;
+z3 = 3847;
+z4 = 947;
+n = 10000;
+
+u = lEcuyer(z1, z2, z3, z4, n);
+
+% (1b) Run's Test
+
+% Perform the runs test
+alpha = 0.05;
+[reject, R] = runsTest(u, alpha);
