d6e9a1bfff8037b6a4f78329aaa9a07ae2b65f1e
Sam Glassman <samglassman@outlook.com>
2024-11-21 18:31:36 +0100

    task1a starting implement

--- code/lEcuyer.m
+++ code/lEcuyer.m
@@ -6,4 +6,27 @@ function u = lEcuyer(z1, z2, z3, z4, n)
     % - bitand
     % - bitxor
     % functions from the Fixed-Point Designer Toolbox. 
+   
+    % We make the following choices for the parameters: 
+    L = 32;     % Word length
+    J = 4;      % Number of generators
+    s1 = 18;    % Values for s taken from Table I for this choice of L and J
+    s2 = 2; 
+    s3 = 7;
+    s4 = 13;
+
+    % allocate space for the n generated variates
+    u = zeros(n, 1); % n x 1 vector to contain the U(0,1) generated numbers
+
+    for i = 1:n
+        % update each lsfr and combine
+        z1 = bitxor(bitsll(z1, s1), bitsrl(z1, L - s1));
+        z2 = bitxor(bitsll(z2, s2), bitsrl(z2, L - s2));
+        z3 = bitxor(bitsll(z3, s3), bitsrl(z3, L - s3));
+        z4 = bitxor(bitsll(z4, s4), bitsrl(z4, L - s4));
+        % TODO: Use vector/array to avoid duplicate code
+        
+
+    end
+
 end
--- code/task1.m
+++ code/task1.m
@@ -8,23 +8,19 @@ clearvars; clcl
 % In (L'Ecuyer, 1999), several approaches for combined LFSRs are given. In this task we will 
 % restrict ourselves to the first example of ME-CF generators as indicated in Table 1 of this paper. 
 % We make the following choices for the parameters: 
-L = 32;
-I = 4;
-s1 = 18;
-s2 = 2; 
-s3 = 7;
-s4 = 13;
-s = [s1, s2, s3, s4];
+
 % Furthermore, we take as seed 𝑧 = 2957, 𝑧 = 646, 𝑧 = 3847, 𝑧 = 947. 
-z1 = 2957; z2 = 646; z3 = 3847; z4 = 947;
-z = [z1, z2, z3, z4];
+%z1 = 2957; z2 = 646; z3 = 3847; z4 = 947;
+% z = [z1, z2, z3, z4];
+
 % (1a) Implement this combined generator in MATLAB and generate 10,000 random numbers with this generator.  
 
-% initialize parameters
-z1 = 2957;
-z2 = 646;
-z3 = 3847;
-z4 = 947;
+% initialize the seeds for the J generators with word length L
+z1 = uint32(2957)
+z2 = uint32(646)
+z3 = uint32(3847)
+z4 = uint32(947)
+% TODO: move to function body. Do the shift values need to be uint32?
 n = 10000;
 
 u = lEcuyer(z1, z2, z3, z4, n);
